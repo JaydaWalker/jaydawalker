0e9ad82c918926b340d7ee260c27f0b7fd2e2538
Jayda Walker <jayda.walker@okstate.edu>
2023-09-12 12:33:40 -0500

    test

--- BasicAppTest/UnitTest1.cs
+++ BasicAppTest/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace BasicAppTest
 		public void TestMethod1()
 		{
 			Class1 tp = new Class1();
-			Assert.AreEqual(tp.Name, "[Jayda Walker] - The Code Master");
+			Assert.AreEqual(tp.Name, "[Jayda Walker] - The Code Master.");
 		}
 	}
 }
